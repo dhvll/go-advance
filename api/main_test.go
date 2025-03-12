95568f1d11f19ce5a4b8a05138e4097c168a8c20
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2025-03-12 15:04:32 +0530

    refactor: replace server initialization with newTestServer in test files

--- api/main_test.go
+++ api/main_test.go
@@ -3,11 +3,22 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/dhvll/go-advance/db/sqlc"
+	"github.com/dhvll/go-advance/util"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmertricKey:  util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+	server := NewServer(config, store)
+	return server
+}
 func TestMain(m *testing.M) {
 
 	gin.SetMode(gin.TestMode)
